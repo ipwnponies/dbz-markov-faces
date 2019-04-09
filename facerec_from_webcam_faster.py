5de18770afe80b925ce81056c9df970458678741
John Nguyen <ipwnponies@gmail.com>
2019-04-08 18:54:55 -0700

    Project boilerplate

---
+++ facerec_from_webcam_faster.py
@@ -0,0 +1,20 @@
+import textwrap
+from functools import lru_cache
+from pathlib import Path
+
+import face_recognition
+import face_recognition.api
+import cv2
+import numpy
+
+import markov_me
+
+DEBUG = False
+
+
+def main():
+    pass
+
+
+if __name__ == main():
+    main()
---
+++ markov_me.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+from functools import lru_cache
+
+import markovify
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
