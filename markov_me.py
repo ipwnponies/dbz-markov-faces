ffc3ed19ea115e30dd09c8af5e043ade85787502
John Nguyen <ipwnponies@gmail.com>
2019-04-08 18:57:41 -0700

    Load data into markovify and set up for sentence generation

--- markov_me.py
+++ markov_me.py
@@ -4,8 +4,30 @@ from functools import lru_cache
 import markovify
 
 
+def generate_sentence():
+    """Generate excellent sentences forever."""
+    text_model = load_data()
+    while True:
+        yield text_model.make_sentence()
+
+
+@lru_cache(maxsize=1)
+def load_data():
+    # Load up subtitle files that have been scrubbed to only have dialogue.
+    sources = Path('./sources/sanitized').glob('**/*.srt')
+    text = ''
+    for source in sources:
+        with source.open() as file:
+            text += file.read()
+
+    text_model = markovify.Text(text)
+    return text_model
+
+
 def main():
-    pass
+    for i in generate_sentence():
+        print(i)
+        input()
 
 
 if __name__ == '__main__':
